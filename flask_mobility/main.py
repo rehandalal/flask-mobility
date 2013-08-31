8eccb4ae43b7bc572dc6053ef8db4d3ec9092e1e
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-08-31 15:43:44 +0200

    Optimized cookie and user agent checking
    Skip checking the regular expression if the cookie is set to on or off.

--- flask_mobility/main.py
+++ flask_mobility/main.py
@@ -28,7 +28,5 @@ class Mobility(object):
         ua = request.user_agent.string.lower()
         mc = request.cookies.get(self.app.config.get('MOBILE_COOKIE'))
 
-        if (self.USER_AGENTS.search(ua) and mc != 'off') or mc == 'on':
-            request.MOBILE = True
-        else:
-            request.MOBILE = False
+        request.MOBILE = (mc == 'on' or 
+            (mc != 'off' and self.USER_AGENTS.search(ua))
