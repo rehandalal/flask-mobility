d739082f1f540ccf1b82a05e5f4bdad3476e5055
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-08-31 23:04:56 +0200

    Removed \ character

--- flask_mobility/main.py
+++ flask_mobility/main.py
@@ -28,5 +28,5 @@ class Mobility(object):
         ua = request.user_agent.string.lower()
         mc = request.cookies.get(self.app.config.get('MOBILE_COOKIE'))
 
-        request.MOBILE = \
-            mc == 'on' or (mc != 'off' and self.USER_AGENTS.search(ua))
+        request.MOBILE = (mc == 'on' or 
+            (mc != 'off' and self.USER_AGENTS.search(ua))
